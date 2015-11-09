f8d4e1ab3bd332d3d766eb588c2c9439eb206a01
Hubert Kowalski <h.kowalski@hakger.pl>
2015-11-09 19:16:00 +0100

    HugePR: update fontawesome, do multiple code updates/deprecates, Fixes #2, Closes #6, Fixes #3, Fixes #9, Closes #10, Fixes #11

--- assets/js/admin-icon-toggle.js
+++ assets/js/admin-icon-toggle.js
@@ -1,10 +1,3 @@
-jQuery( document ).ready( function ( e ) {
-	var newIconObj, oldIconObj;
-	jQuery( '#feature-icon' ).find( '.feature-icon-selector' ).change( function ( e ) {
-		oldIconObj = jQuery( '#feature-icon' ).find( '.currently-selected-icon' );
-		newIconObj = jQuery( this );
-
-		jQuery( '.icon-preview' ).removeClass( oldIconObj.val() ).addClass( newIconObj.val() );
-		oldIconObj.val( newIconObj.val() );
-	} );
+jQuery( document ).ready( function ( e ) {	
+    jQuery( '.feature-icon-color' ).wpColorPicker();  
 });
